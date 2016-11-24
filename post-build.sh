ecd32c9263e9cbc93137b80482d1f8c9bc01382b
Thomas Chen <tkchen@gmail.com>
2016-11-24 00:08:07 -0800

    Fix cmake configuration

--- post-build.sh
+++ post-build.sh
@@ -7,6 +7,7 @@ PY="${BIN}/../PyCell/"
 cp -r ${PY}Plugins/* ${MOD}
 cp ${BIN}/${FILE} ${EXT}
 install_name_tool -id ./Contents/Resources/.kivy/extensions/plugins/${FILE} ${EXT}${FILE}
+echo "PyCell installed to $EXT"
 #install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.5/Python @executable_path/../Frameworks/python/3.5.0/Python ${DEST}${FILE}
 #install_name_tool -change /usr/local/opt/boost-python/lib/libboost_python3.dylib @executable_path/lib/libboost_python3.dylib ${EXT}${FILE}
 #install_name_tool -change /usr/local/opt/libiomp/lib/libiomp5.dylib @executable_path/lib/libiomp5.dylib ${EXT}${FILE}
